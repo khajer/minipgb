920bb178011b554a9ec9224d12e841b476e85493
itsara <khajer@gmail.com>
2024-10-30 23:28:48 +0700

    create simple module to test concept

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+pub mod minipgb {
+    pub struct MiniPgb {
+        pub value: i32,
+    }
+    pub fn new() -> MiniPgb {
+        MiniPgb { value: 0 }
+    }
+    impl MiniPgb {
+        fn start() {}
+        fn finish() {}
+    }
+}
+
 fn main() {
     println!("hello, world");
 }
