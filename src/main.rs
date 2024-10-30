415254322bc76bed4cce851bb8f29c49320d63f0
itsara <khajer@gmail.com>
2024-10-31 01:08:15 +0700

    Poc concept parameter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 pub mod minipgb {
+    use ansi_term::Color;
+    use terminal_size::{terminal_size, Height, Width};
+
     pub struct MiniPgb {
         pub value: i32,
     }
@@ -6,11 +9,30 @@ pub mod minipgb {
         MiniPgb { value: 0 }
     }
     impl MiniPgb {
-        fn start() {}
-        fn finish() {}
+        pub fn start(self) {
+            let size = terminal_size();
+            if let Some((Width(w), Height(h))) = size {
+                println!("Your terminal is {} cols wide and {} lines tall", w, h);
+                let val_progress = "";
+                let mut text = "".to_string();
+                let prefix = "download file : test.png";
+                let template = format!("{}[]", prefix);
+                for _ in val_progress.len()..(w as usize) - template.len() {
+                    text.insert(0, ' ');
+                }
+                println!("{}[{}]", prefix, text)
+            } else {
+                println!("Unable to get terminal size");
+            }
+
+            println!("{}", Color::Red.paint("This text is red!"));
+        }
+        pub fn finish(self) {}
     }
 }
 
 fn main() {
     println!("hello, world");
+    let pg = minipgb::new();
+    pg.start();
 }
