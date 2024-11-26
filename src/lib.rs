26cadfadc4173c845d7f456888964ce6773d110c
itsara <khajer@gmail.com>
2024-11-26 22:00:36 +0700

    refactor code

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,6 @@ use std::process;
 use terminal_size::{terminal_size, Height, Width};
 
 pub mod minipgb {
-    // use ansi_term::Color;
-    // use terminal_size::{terminal_size, Height, Width};
-    //
     use crate::draw;
 
     pub struct MiniPgb {
@@ -16,7 +13,7 @@ pub mod minipgb {
     }
     impl MiniPgb {
         pub fn start(&self) {
-            draw(self.value);            
+            draw(self.value);
         }
         pub fn inc(&mut self, n: usize) {
             if self.value < 100 {
@@ -31,14 +28,13 @@ pub mod minipgb {
 
 fn draw(pg_value: usize) {
     let size = terminal_size();
-    if let Some((Width(w), Height(_h))) = size {                
-        let prefix = "Download File > download.png ";        
-        let len = prefix.len();        
-        let tatal_txt_100 = w as usize - prefix.len() - "[]".len();        
-        let pg  = (pg_value as f32/100.0) * tatal_txt_100 as f32;
-        let space = tatal_txt_100 - pg as usize;        
-        let  text = format!("{}{}", "#".repeat(pg as usize), " ".repeat(space));        
-        print!("\r{}[{}]", prefix, text);        
+    if let Some((Width(w), Height(_h))) = size {
+        let prefix = "Download File > download.png ";
+        let tatal_txt_100 = w as usize - prefix.len() - "[]".len();
+        let pg = (pg_value as f32 / 100.0) * tatal_txt_100 as f32;
+        let space = tatal_txt_100 - pg as usize;
+        let text = format!("{}{}", "#".repeat(pg as usize), " ".repeat(space));
+        print!("\r{}[{}]", prefix, text);
     } else {
         println!("Unable to get terminal size");
         process::exit(0x0100);
