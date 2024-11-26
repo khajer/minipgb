7cea45809c3940d34e2932ed61f550abe53c3533
Ubuntu <ubuntu@ip-172-31-1-88.ap-southeast-1.compute.internal>
2024-11-26 10:10:15 +0000

    set number completed

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use std::{thread};
+
 fn main() {
     println!("hello, world");
     let mut pg = minipgb::minipgb::new();
     pg.start();
-    for _i in 1..100 {
+    
+    for _i in 1..101 {
         pg.inc(1);
+        // thread::sleep_ms(100);
+        
+
     }
 }
